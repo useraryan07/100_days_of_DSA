dc77d28c4d19f51152bfcbce30bf6e3b03f4418e
Aryan Budhija <Aryan.25284@stu.upes.ac.in>
2026-04-25 10:51:29 +0530

    Implement max and min element finder in array

---
+++ day17.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int max = arr[0];
+    int min = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    printf("Maximum element: %d\n", max);
+    printf("Minimum element: %d\n", min);
+    return 0;
+}
